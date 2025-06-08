5d2d21728a33222f0c8fdb04d138ee0929890c85
crappyo1 <crappyo1@outlook.com>
2025-06-08 10:02:11 +0000

    1) Created sataliteColumnMapping table. 2) Create SP GetColumnMapping

--- tables/CreateControlSatTable.sql
+++ tables/CreateControlSatTable.sql
@@ -1,3 +1,10 @@
+/****** Object:  Schema [Control].[SourceObject] ******/
+/*
+This script creates the SourceObject table in the Control schema.
+It is used to store metadata about sink objects in data integration tasks.
+The Name collumn is used to identify the object and is currently a concatenation <Schema Name>.<Object Name>.
+Other details have yet to be defined.
+*/
 IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[Control].[SourceObject]') AND type in (N'U'))
 DROP TABLE [Control].[SourceObject]
 GO
@@ -15,6 +22,13 @@ ALTER TABLE [Control].[SourceObject] ADD PRIMARY KEY CLUSTERED
 GO
 
 
+/****** Object:  Table [Control].[SinkObject] ******/
+/*
+This script creates the SinkObject table in the Control schema.
+It is used to store metadata about sink objects in data integration tasks.
+The Name collumn is used to identify the object and is currently a concatenation <Schema Name>.<Object Name>.
+Other details have yet to be defined.
+*/
 IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[Control].[SinkObject]') AND type in (N'U'))
 DROP TABLE [Control].[SinkObject]
 GO
@@ -32,6 +46,39 @@ ALTER TABLE [Control].[SinkObject] ADD PRIMARY KEY CLUSTERED
 GO
 
 
+/****** Object:  Table [Control].[ColumnMapping] ******/
+/*
+This script creates the ColumnMapping table in the Control schema.
+It is used to map source columns to target columns in data integration tasks.
+The table includes columns for the mapping ID, source and target column names, types, physical types, and an ordinal value to maintain the order of the mappings.
+The types and physical types are necessary for use with the TabularTranslator in Azure Data Factory or Synapse pipelines.
+The MappingId is used to associate the column mapping with a specific data integration task.
+*/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[Control].[ColumnMapping]') AND type in (N'U'))
+DROP TABLE [Control].[ColumnMapping]
+GO
+CREATE TABLE [Control].[ColumnMapping](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[SourceColumn] [nvarchar](64) NOT NULL,
+    [SourceType] [nvarchar](64) NOT NULL,
+    [SourcePhysicalType] [nvarchar](64) NOT NULL,
+	[TargetColumn] [nvarchar](64) NOT NULL,
+    [TargetType] [nvarchar](64) NOT NULL,
+    [TargetPhysicalType] [nvarchar](64) NOT NULL,
+    [MappingId] [int] NOT NULL,
+    [Ordinal] [int] NOT NULL
+) ON [PRIMARY] 
+GO
+ALTER TABLE [Control].[ColumnMapping] ADD PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+GO
+
+
+/*
+Test data to be inserted into the Satalite tables.
+*/
 DECLARE @ObjectMetadata NVARCHAR(max)  = N'[
     {
         "Name": "SalesLT.Address",
@@ -68,4 +115,415 @@ SELECT * FROM OPENJSON(@ObjectMetadata)
     WITH ([Name] [nvarchar](256),
     [SchemaName] [varchar](64),
     [ObjectName] [nvarchar](64))
+GO
+
+
+
+DECLARE @CA_MappingMetadata NVARCHAR(max)  = N'[
+    {
+        "source": {
+            "name": "CustomerID",
+            "type": "Int32",
+            "physicalType": "int"
+        },
+        "sink": {
+            "name": "CustomerID",
+            "type": "Int32",
+            "physicalType": "int"
+        }
+    },
+    {
+        "source": {
+            "name": "AddressID",
+            "type": "Int32",
+            "physicalType": "int"
+        },
+        "sink": {
+            "name": "AddressID",
+            "type": "Int32",
+            "physicalType": "int"
+        }
+    },
+    {
+        "source": {
+            "name": "AddressType",
+            "type": "String",
+            "physicalType": "nvarchar"
+        },
+        "sink": {
+            "name": "AddressType",
+            "type": "String",
+            "physicalType": "nvarchar"
+        }
+    },
+    {
+        "source": {
+            "name": "rowguid",
+            "type": "Guid",
+            "physicalType": "uniqueidentifier"
+        },
+        "sink": {
+            "name": "rowguid",
+            "type": "Guid",
+            "physicalType": "uniqueidentifier"
+        }
+    },
+    {
+        "source": {
+            "name": "ModifiedDate",
+            "type": "DateTime",
+            "physicalType": "datetime"
+        },
+        "sink": {
+            "name": "ModifiedDate",
+            "type": "DateTime",
+            "physicalType": "datetime2"
+        }
+    }
+]'
+
+DECLARE @C_MappingMetadata NVARCHAR(max)  = N'[
+    {
+        "source": {
+            "name": "CustomerID",
+            "type": "Int32",
+            "physicalType": "int"
+        },
+        "sink": {
+            "name": "CustomerID",
+            "type": "Int32",
+            "physicalType": "int"
+        }
+    },
+    {
+        "source": {
+            "name": "NameStyle",
+            "type": "Boolean",
+            "physicalType": "bit"
+        },
+        "sink": {
+            "name": "NameStyle",
+            "type": "Boolean",
+            "physicalType": "bit"
+        }
+    },
+    {
+        "source": {
+            "name": "Title",
+            "type": "String",
+            "physicalType": "nvarchar"
+        },
+        "sink": {
+            "name": "Title",
+            "type": "String",
+            "physicalType": "nvarchar"
+        }
+    },
+    {
+        "source": {
+            "name": "FirstName",
+            "type": "String",
+            "physicalType": "nvarchar"
+        },
+        "sink": {
+            "name": "FirstName",
+            "type": "String",
+            "physicalType": "nvarchar"
+        }
+    },
+    {
+        "source": {
+            "name": "MiddleName",
+            "type": "String",
+            "physicalType": "nvarchar"
+        },
+        "sink": {
+            "name": "MiddleName",
+            "type": "String",
+            "physicalType": "nvarchar"
+        }
+    },
+    {
+        "source": {
+            "name": "LastName",
+            "type": "String",
+            "physicalType": "nvarchar"
+        },
+        "sink": {
+            "name": "LastName",
+            "type": "String",
+            "physicalType": "nvarchar"
+        }
+    },
+    {
+        "source": {
+            "name": "PasswordSalt",
+            "type": "String",
+            "physicalType": "varchar"
+        },
+        "sink": {
+            "name": "PasswordSalt",
+            "type": "String",
+            "physicalType": "nvarchar"
+        }
+    },
+    {
+        "source": {
+            "name": "rowguid",
+            "type": "Guid",
+            "physicalType": "uniqueidentifier"
+        },
+        "sink": {
+            "name": "rowguid",
+            "type": "Guid",
+            "physicalType": "uniqueidentifier"
+        }
+    },
+    {
+        "source": {
+            "name": "ModifiedDate",
+            "type": "DateTime",
+            "physicalType": "datetime"
+        },
+        "sink": {
+            "name": "ModifiedDate",
+            "type": "DateTime",
+            "physicalType": "datetime2"
+        }
+    },
+    {
+        "source": {
+            "name": "Suffix",
+            "type": "String",
+            "physicalType": "nvarchar"
+        },
+        "sink": {
+            "name": "Suffix",
+            "type": "String",
+            "physicalType": "nvarchar"
+        }
+    },
+    {
+        "source": {
+            "name": "CompanyName",
+            "type": "String",
+            "physicalType": "nvarchar"
+        },
+        "sink": {
+            "name": "CompanyName",
+            "type": "String",
+            "physicalType": "nvarchar"
+        }
+    },
+    {
+        "source": {
+            "name": "SalesPerson",
+            "type": "String",
+            "physicalType": "nvarchar"
+        },
+        "sink": {
+            "name": "SalesPerson",
+            "type": "String",
+            "physicalType": "nvarchar"
+        }
+    },
+    {
+        "source": {
+            "name": "EmailAddress",
+            "type": "String",
+            "physicalType": "nvarchar"
+        },
+        "sink": {
+            "name": "EmailAddress",
+            "type": "String",
+            "physicalType": "nvarchar"
+        }
+    },
+    {
+        "source": {
+            "name": "Phone",
+            "type": "String",
+            "physicalType": "nvarchar"
+        },
+        "sink": {
+            "name": "Phone",
+            "type": "String",
+            "physicalType": "nvarchar"
+        }
+    },
+    {
+        "source": {
+            "name": "PasswordHash",
+            "type": "String",
+            "physicalType": "varchar"
+        },
+        "sink": {
+            "name": "PasswordHash",
+            "type": "String",
+            "physicalType": "nvarchar"
+        }
+    }
+]'
+
+DECLARE @A_MappingMetadata NVARCHAR(max)  = N'[
+    {
+        "source": {
+            "name": "AddressID",
+            "type": "Int32",
+            "physicalType": "int"
+        },
+        "sink": {
+            "name": "AddressID",
+            "type": "Int32",
+            "physicalType": "int"
+        }
+    },
+    {
+        "source": {
+            "name": "AddressLine1",
+            "type": "String",
+            "physicalType": "nvarchar"
+        },
+        "sink": {
+            "name": "AddressLine1",
+            "type": "String",
+            "physicalType": "nvarchar"
+        }
+    },
+    {
+        "source": {
+            "name": "AddressLine2",
+            "type": "String",
+            "physicalType": "nvarchar"
+        },
+        "sink": {
+            "name": "AddressLine2",
+            "type": "String",
+            "physicalType": "nvarchar"
+        }
+    },
+    {
+        "source": {
+            "name": "City",
+            "type": "String",
+            "physicalType": "nvarchar"
+        },
+        "sink": {
+            "name": "City",
+            "type": "String",
+            "physicalType": "nvarchar"
+        }
+    },
+    {
+        "source": {
+            "name": "StateProvince",
+            "type": "String",
+            "physicalType": "nvarchar"
+        },
+        "sink": {
+            "name": "StateProvince",
+            "type": "String",
+            "physicalType": "nvarchar"
+        }
+    },
+    {
+        "source": {
+            "name": "CountryRegion",
+            "type": "String",
+            "physicalType": "nvarchar"
+        },
+        "sink": {
+            "name": "CountryRegion",
+            "type": "String",
+            "physicalType": "nvarchar"
+        }
+    },
+    {
+        "source": {
+            "name": "PostalCode",
+            "type": "String",
+            "physicalType": "nvarchar"
+        },
+        "sink": {
+            "name": "PostalCode",
+            "type": "String",
+            "physicalType": "nvarchar"
+        }
+    },
+    {
+        "source": {
+            "name": "rowguid",
+            "type": "Guid",
+            "physicalType": "uniqueidentifier"
+        },
+        "sink": {
+            "name": "rowguid",
+            "type": "Guid",
+            "physicalType": "uniqueidentifier"
+        }
+    },
+    {
+        "source": {
+            "name": "ModifiedDate",
+            "type": "DateTime",
+            "physicalType": "datetime"
+        },
+        "sink": {
+            "name": "ModifiedDate",
+            "type": "DateTime",
+            "physicalType": "datetime2"
+        }
+    }
+]'
+
+INSERT INTO [Control].[ColumnMapping] (
+    [SourceColumn],
+    [SourceType],
+    [SourcePhysicalType],
+	[TargetColumn],
+    [TargetType],
+    [TargetPhysicalType],
+    [MappingId],
+    [Ordinal])
+SELECT
+    *,
+    1,
+    0
+FROM OPENJSON(@A_MappingMetadata)
+WITH (
+    [SourceColumn] [nvarchar](64) '$.source.name',
+    [SourceType] [nvarchar](64) '$.source.type',
+    [SourcePhysicalType] [nvarchar](64) '$.source.physicalType',
+    [TargetColumn] [nvarchar](64) '$.sink.name',
+    [TargetType] [nvarchar](64) '$.sink.type',
+    [TargetPhysicalType] [nvarchar](64) '$.sink.physicalType'
+)
+UNION ALL
+SELECT
+    *,
+    2,
+    0
+FROM OPENJSON(@C_MappingMetadata)
+WITH (
+    [SourceColumn] [nvarchar](64) '$.source.name',
+    [SourceType] [nvarchar](64) '$.source.type',
+    [SourcePhysicalType] [nvarchar](64) '$.source.physicalType',
+    [TargetColumn] [nvarchar](64) '$.sink.name',
+    [TargetType] [nvarchar](64) '$.sink.type',
+    [TargetPhysicalType] [nvarchar](64) '$.sink.physicalType'
+)
+UNION ALL
+SELECT
+    *,
+    3,
+    0
+FROM OPENJSON(@CA_MappingMetadata)
+WITH (
+    [SourceColumn] [nvarchar](64) '$.source.name',
+    [SourceType] [nvarchar](64) '$.source.type',
+    [SourcePhysicalType] [nvarchar](64) '$.source.physicalType',
+    [TargetColumn] [nvarchar](64) '$.sink.name',
+    [TargetType] [nvarchar](64) '$.sink.type',
+    [TargetPhysicalType] [nvarchar](64) '$.sink.physicalType'
+)
 GO
--- tables/CreateStoredProcedure.sql
+++ tables/CreateStoredProcedure.sql
@@ -26,3 +26,30 @@ CREATE PROCEDURE [Control].[InsertTaskStatus]
 AS
     INSERT INTO [Control].[TaskStatus] VALUES (@TaskId, @Status, @ErrorMessage, (GETDATE() AT TIME ZONE 'UTC' AT TIME ZONE 'Singapore Standard Time'))
 GO
+
+
+DROP PROCEDURE IF EXISTS [Control].[GetColumnMapping]
+GO
+CREATE PROCEDURE [Control].[GetColumnMapping]
+  @MappingId [int] 
+AS
+BEGIN
+    DECLARE @JSON_CONSTRUCT varchar(MAX) = '{ "type": "TabularTranslator", "mappings": {X} }';
+    DECLARE @JSON_MAPPING VARCHAR(MAX);
+
+    SET @JSON_MAPPING = (
+        Select 
+        CM.SourceColumn as 'source.name',
+        CM.SourceType as 'source.type',
+        CM.SourcePhysicalType as 'source.physicalType',
+        CM.TargetColumn as 'sink.name',
+        CM.TargetType as 'sink.type',
+        CM.TargetPhysicalType as 'sink.physicalType'
+        from Control.ColumnMapping AS CM
+        WHERE CM.MappingId = @MappingId
+        FOR JSON PATH
+    );
+
+    SELECT REPLACE(@JSON_CONSTRUCT, '{X}', @JSON_MAPPING) AS JSON_OUTPUT;
+END
+GO
